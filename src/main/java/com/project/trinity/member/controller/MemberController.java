63f697ce82b2e6f9af2ba3fdfd230b55710f9462
korjamjam <j9k6i0j7jh@gmail.com>
2024-11-06 18:25:55 +0900

    기본적인 member, board의 controller, service, dao, vo 클래스 셋팅

---
+++ src/main/java/com/project/trinity/member/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.project.trinity.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@Controller
+public class MemberController {
+
+}
