84bc54ff896a25a23c99dab8d90620ab290469a4
taeyoung-oh <oty0313@gamil.com>
2024-11-13 17:41:28 +0900

    매핑 변경

--- src/main/java/com/project/trinity/hospital/controller/HospitalController.java
+++ src/main/java/com/project/trinity/hospital/controller/HospitalController.java
@@ -1,15 +1,11 @@
 package com.project.trinity.hospital.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.project.trinity.hospital.model.vo.Hospital;
-import com.project.trinity.hospital.service.HospitalService;
-
-@Controller("/hospital")
+@Controller
+@RequestMapping("/trinity/hospital")
 public class HospitalController {
 	@GetMapping("/list")
 	public String hospitalList() {
