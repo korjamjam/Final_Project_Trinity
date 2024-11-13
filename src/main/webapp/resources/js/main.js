22d344e3d37128a6c347f4b72c78c7c247488f33
taeyoung-oh <oty0313@gamil.com>
2024-11-13 10:49:08 +0900

    로그아웃

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,3 +1,9 @@
 function checkLogin(){
     location.href = "general_reservation"
 }
+document.addEventListener("DOMContentLoaded", function() {
+	
+    if (alertMsg) { // alertMsg가 비어 있지 않으면 알림을 띄움
+        alert(alertMsg);
+    }
+});
