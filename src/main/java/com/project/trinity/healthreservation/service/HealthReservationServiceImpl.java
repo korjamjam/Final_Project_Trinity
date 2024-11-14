535e49db6b4c5c8f603df29bf3ba355ad0108402
korjamjam <j9k6i0j7jh@gmail.com>
2024-11-14 20:42:24 +0900

    백신예약 기본 셋팅

---
+++ src/main/java/com/project/trinity/healthreservation/service/HealthReservationServiceImpl.java
@@ -0,0 +1,5 @@
+package com.project.trinity.healthreservation.service;
+
+public class HealthReservationServiceImpl implements HealthReservationService {
+
+}
--- src/main/java/com/project/trinity/hospital_detail/controller/HealthReservationSerivceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.project.trinity.hospital_detail.controller;
-
-public class HealthReservationSerivceImpl implements HealthReservationSerivce{
-
-}
