ecfcf18b090b3b04fb637ed84ac5b471bd00796d
taeyoung-oh <oty0313@gamil.com>
2024-11-15 12:24:26 +0900

    병원 리스트 제작중

--- src/main/java/com/project/trinity/hospital/service/HospitalService.java
+++ src/main/java/com/project/trinity/hospital/service/HospitalService.java
@@ -1,9 +1,13 @@
 package com.project.trinity.hospital.service;
 
+import java.util.ArrayList;
+
 import com.project.trinity.hospital.model.vo.Hospital;
 
 public interface HospitalService {
 
 	Hospital selectHospital(String hpId);
+
+	ArrayList<Hospital> selectHospitalList(String subject, String order);
 
 }
