c1ee316216f2e055e24f8f778d539545f6b3f18d
korjamjam <j9k6i0j7jh@gmail.com>
2024-11-15 16:28:52 +0900

    healthreservation DB적용전

--- src/main/java/com/project/trinity/healthreservation/controller/HealthReservationController.java
+++ src/main/java/com/project/trinity/healthreservation/controller/HealthReservationController.java
@@ -1,7 +1,10 @@
 package com.project.trinity.healthreservation.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,23 +16,40 @@ import com.project.trinity.reservation.model.vo.Reservation;
 @Controller
 @RequestMapping("/healthReservation")
 public class HealthReservationController {
-	
-	@Autowired
+
+    @Autowired
     private HealthReservationService healthReservationService;
-	
-	//백신 예약 페이지1
-		@GetMapping("/vaccinepage1")
-	    public String vaccineReservation1() {
-	        return "health_reservation/vaccine_reservation1";
-	    }
-	    
-		//백신 예약 페이지2
-	    @GetMapping("/vaccinepage2")
-	    public String vaccineReservation2() {
-	        return "health_reservation/vaccine_reservation2";
-	    }
-	    
+
+    // 백신 예약 페이지 1로 이동
+    @GetMapping("/vaccinepage1")
+    public String vaccineReservation1() {
+        return "health_reservation/vaccine_reservation1";
+    }
+
+    // 백신 예약 페이지 2로 이동
+    @PostMapping("/vaccinepage2")
+    public String vaccineReservation2(@ModelAttribute Reservation reservation, Model model) {
+        model.addAttribute("reservation", reservation);
+        return "health_reservation/vaccine_reservation2";
+    }
+
+    @PostMapping("/submitReservation")
+    public String submitReservation(@ModelAttribute Reservation reservation, HttpSession session) {
+        // 세션에서 USER_NO 가져오기
+        Integer userNo = (Integer) session.getAttribute("userNo");
+
+        if (userNo == null) {
+            throw new IllegalArgumentException("로그인된 사용자 정보가 없습니다.");
+        }
+        // USER_NO 설정
+        reservation.setUserNo(userNo);
+        // 예약 정보 저장
+        healthReservationService.saveReservation(reservation);
+        return "redirect:/healthReservation/vaccinepage1"; // 예약 완료 페이지
+    }
+
 }
+
 //	@GetMapping("/guide")
 //	public String healthReservationGuide() {
 //		return "health_reservation/health_reservation_guide";
--- src/main/java/com/project/trinity/hospital/model/dao/HospitalDao.java
+++ src/main/java/com/project/trinity/hospital/model/dao/HospitalDao.java
@@ -1,7 +1,6 @@
 package com.project.trinity.hospital.model.dao;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,13 +10,9 @@ import com.project.trinity.hospital.model.vo.Hospital;
 @Repository
 public class HospitalDao {
 
-	public Hospital selectHospital(SqlSessionTemplate sqlSession, String hpId) {
-		return sqlSession.selectOne("hospitalMapper.selectHospital", hpId);
-	}
-
 	public ArrayList<Hospital> selectHospitalList(SqlSessionTemplate sqlSession, String subject, String order) {
-		Map<String, String> map = new Map(subject, order);
-		return sqlSession.selectMap("hospitalMapper.selectHospitalList", map);
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
