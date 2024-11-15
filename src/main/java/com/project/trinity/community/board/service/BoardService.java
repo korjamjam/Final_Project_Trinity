763e797c6d941c5114647cad803749c22f6671ef
korjamjam <j9k6i0j7jh@gmail.com>
2024-11-15 11:20:44 +0900

    Revert "Merge branch 'main' of https://github.com/korjamjam/Final_Project_Trinity into JeonJaemin"
    This reverts commit f1773e1ea55f2d843392108e6f0dc69018fb08d8, reversing
    changes made to e76774bc936d3e36bd325a235dd5c75aa83eb08f.

--- src/main/java/com/project/trinity/community/board/service/BoardService.java
+++ src/main/java/com/project/trinity/community/board/service/BoardService.java
@@ -1,22 +1,42 @@
 package com.project.trinity.community.board.service;
 
 import java.util.ArrayList;
+
 import com.project.trinity.community.board.model.vo.Board;
 import com.project.trinity.community.board.model.vo.Reply;
 import com.project.trinity.community.common.vo.PageInfo;
 
-
 public interface BoardService {
-
-    int insertBoard(Board board);
-    Board selectBoard(int boardNo);
-    int updateBoard(Board board);
-    int deleteBoard(int boardNo);
-    int selectListCount();
-    int increaseCount(int bno);
-    ArrayList<Reply> selectReply(int bno);
-    int insertReply(Reply r);
-    ArrayList<Board> selectTopBoardList();
-	ArrayList<Board> selectList(PageInfo pi, String sortType);
+	//게시글 총 갯수 가져오기
+	int selectListCount();
+	
+	//게시글 목록 가져오기
+	ArrayList<Board> selectList(PageInfo pi);
+	
+	//게시글 조회수 증가
+	int increaseCount(int bno);
+	
+	//boardNo로 게시글 조회
+	Board selectBoard(int bno);
+	
+	//게시글 추가(insert) 
+	int insertBoard(Board b);
+	
+	//게시글 수정
+	int updateBoard(Board b);
+	
+	//댓글목록 가져오기
+	ArrayList<Reply> selectReply(int bno);
+	
+	//댓글 추가
+	int insertReply(Reply r);
+	
+	//조회수 상위 5개
+	public ArrayList<Board> selectTopBoardList();
 }
+
+
+
+
+
 
--- src/main/java/com/project/trinity/hospital/service/HospitalService.java
+++ src/main/java/com/project/trinity/hospital/service/HospitalService.java
@@ -1,13 +1,9 @@
 package com.project.trinity.hospital.service;
 
-import java.util.ArrayList;
-
 import com.project.trinity.hospital.model.vo.Hospital;
 
 public interface HospitalService {
 
 	Hospital selectHospital(String hpId);
-
-	ArrayList<Hospital> selectHospitalList(String list_subject, String list_orderBy);
 
 }
