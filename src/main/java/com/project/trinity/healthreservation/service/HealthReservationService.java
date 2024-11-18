3346a8d6f5a9d8e22e8b1cfe7659dc2debb9e2dd
minseo1999 <assd092728@gmail.com>
2024-11-18 19:14:54 +0900

    비회원 추가

--- src/main/java/com/project/trinity/healthreservation/service/HealthReservationService.java
+++ src/main/java/com/project/trinity/healthreservation/service/HealthReservationService.java
@@ -1,8 +1,10 @@
 package com.project.trinity.healthreservation.service;
 
+import com.project.trinity.member.model.vo.Guest;
 import com.project.trinity.reservation.model.vo.Reservation;
 
 public interface HealthReservationService {
     void saveReservation(Reservation reservation);
     Reservation getReservationWithVaccine(int resNo);
+	int insertGuest(Guest guest);
 }
