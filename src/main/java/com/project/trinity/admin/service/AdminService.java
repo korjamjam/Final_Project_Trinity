e8e778abefed7170ee728d47dd8c5d389f7e0c94
korjamjam <j9k6i0j7jh@gmail.com>
2024-11-29 17:47:47 +0900

    등업상세페이지 구현

--- src/main/java/com/project/trinity/admin/service/AdminService.java
+++ src/main/java/com/project/trinity/admin/service/AdminService.java
@@ -9,4 +9,11 @@ public interface AdminService {
     List<Member> getAllMembers(); // 모든 회원을 가져오는 메서드 선언
     
     List<Rankup> getAllRankups(); // 등업 신청 목록 조회
+    
+    public Rankup getRankupDetail(int seqNo);
+    
+    void approveRankup(int seqNo);
+
+    void rejectRankup(int seqNo);
+    
 }
