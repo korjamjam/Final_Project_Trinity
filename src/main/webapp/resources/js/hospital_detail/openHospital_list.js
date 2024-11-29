db41d7ebbdec6edd43e5253b07312130b47df08b
taeyoung-oh <oty0313@gamil.com>
2024-11-29 09:19:49 +0900

    .

--- src/main/webapp/resources/js/hospital_detail/openHospital_list.js
+++ src/main/webapp/resources/js/hospital_detail/openHospital_list.js
@@ -85,3 +85,36 @@ const func = function () {
         });
     }
 };
+
+// selectbox를 골랐을 때 $(".hospitalList_list_wrapper2").remove();
+function replaceHospitalList(hospitals) {
+    $(".hospitalList_list_wrapper2").remove();
+    $(".hospitalList_list").remove();
+    //여기서부터 데이터 가지고 오기
+    
+    hospitals.forEach(hospital => {
+        $('.hospitalList_wrapper').append(`
+            <div class="hospitalList_list_wrapper2">
+                <div class="hospitalList_list">
+                    <div class="hospitalList_listTitle">
+                        <a href="${contextPath}/hospital/detail?hosNo=${hospital.hosNo}">
+                            <p>${hospital.hosName}</p>
+                        </a>
+                    </div>
+                    <div class="hospitalList_listOpenTime">
+                        <span>평일 ${hospital.hosStartTime1} ~ ${hospital.hosEndTime1}</span>
+                        <span>${hospital.department}</span>
+                    </div>
+                    <div class="hospitalList_listAddress">
+                        <p>${hospital.hosAddress}</p>
+                    </div>
+                    <div class="hospitalList_listTag">
+                        <button>어린이 국가예방접종</button>
+                        <button>영유아 검진</button>
+                        <button>주차장</button>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+}
