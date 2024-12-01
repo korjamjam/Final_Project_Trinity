8825790f78ee16de1307bd2cdcb07c3cdeb48bc1
jjhim0531 <jjhim531@naver.com>
2024-12-02 00:09:21 +0900

    241201home

--- src/main/java/com/project/trinity/community/board/model/vo/Board.java
+++ src/main/java/com/project/trinity/community/board/model/vo/Board.java
@@ -14,6 +14,7 @@ public class Board {
     private String boardNo;        // 게시글 번호
     private int boardType;         // 게시글 유형
     private String boardTitle;     // 게시글 제목
+    private String tag;
     private String userNo;         // 작성자의 고유 번호 (로그인 사용자와 연결)
     private String boardWriter;    // 작성자의 ID (조회 시 MEMBER 테이블에서 가져옴)
     private String boardContent;   // 게시글 내용
