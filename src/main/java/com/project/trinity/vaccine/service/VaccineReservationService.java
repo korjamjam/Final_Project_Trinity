ad8b0db68a4556dd4d146291d9d02eee9f940766
ckc980515@gmail.com <ckc980515@gmail.com>
2024-12-04 11:19:39 +0900

    백신예약

--- src/main/java/com/project/trinity/vaccine/service/VaccineReservationService.java
+++ src/main/java/com/project/trinity/vaccine/service/VaccineReservationService.java
@@ -8,21 +8,23 @@ import com.project.trinity.vaccine.model.vo.VaccineReservation;
 
 public interface VaccineReservationService {
 
-    /**
-     * 예약 정보를 데이터베이스에 삽입하는 메서드
-     *
-     * @param vaccineReservation 예약 정보가 담긴 VaccineReservation 객체
-     */
-    void insertReservation(VaccineReservation vaccineReservation); // 예약 삽입
+	/**
+	 * 예약 정보를 데이터베이스에 삽입하는 메서드
+	 *
+	 * @param vaccineReservation 예약 정보가 담긴 VaccineReservation 객체
+	 */
+	void insertReservation(VaccineReservation vaccineReservation); // 예약 삽입
 
-    /**
-     * 모든 병원 목록을 조회하는 메서드
-     *
-     * @return 병원 정보가 담긴 HospitalInfo 객체의 리스트
-     */
-    List<HospitalInfo> getAllHospitals(); // 병원 목록 조회
-    
-    String insertGuest(Guest guest); // 비회원 정보를 삽입하고 GST_NO 반환
+	/**
+	 * 모든 병원 목록을 조회하는 메서드
+	 *
+	 * @return 병원 정보가 담긴 HospitalInfo 객체의 리스트
+	 */
+	List<HospitalInfo> getAllHospitals(); // 병원 목록 조회
+
+	String insertGuest(Guest guest); // 비회원 정보를 삽입하고 GST_NO 반환
 
 	VaccineReservation selectReservation(String resNo);
+
+	 List<VaccineReservation> getReservationsByUserNo(String userNo); // 정적 선언 아님
 }
